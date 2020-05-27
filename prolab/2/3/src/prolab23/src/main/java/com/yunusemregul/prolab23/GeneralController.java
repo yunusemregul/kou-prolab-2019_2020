fbd3b645d1e3c8e47c64cb481369a9bfd2340f4a
Yunus Emre Gül <yunemregul@gmail.com>
2020-05-27 18:07:31 +0300

    Create database, update controllers

---
+++ prolab/2/3/src/prolab23/src/main/java/com/yunusemregul/prolab23/GeneralController.java
@@ -0,0 +1,37 @@
+package com.yunusemregul.prolab23;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class GeneralController implements Initializable
+{
+    public GeneralController()
+    {
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        
+    }
+    
+    @FXML
+    public void close(ActionEvent e)
+    {
+        Stage stage = (Stage)((Button)e.getSource()).getScene().getWindow();
+        stage.close();
+    }
+    
+    @FXML
+    public void minimize(ActionEvent e)
+    {
+        Stage stage = (Stage)((Button)e.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+}
--- prolab/2/3/src/prolab23/src/main/java/com/yunusemregul/prolab23/LoginController.java
+++ prolab/2/3/src/prolab23/src/main/java/com/yunusemregul/prolab23/LoginController.java
@@ -1,8 +1,18 @@
 package com.yunusemregul.prolab23;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-public class LoginController {
+public class LoginController extends GeneralController
+{
+    public LoginController()
+    {
 
+    }
+    
+    public void openKayitOl(ActionEvent e)
+    {
+         
+    }
 }
